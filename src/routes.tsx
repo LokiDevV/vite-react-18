9abf1acd41a5a84e9ed0894f0a01bd2145841f81
luongdd <luongdd@vnext.com.vn>
2022-07-12 15:41:03 +0700

    update config

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Posts from './pages/posts';
+import Post from './pages/posts/post';
 
 const Routing = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="teams" element={<Posts />}>
+          <Route path=":postId" element={<Post />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
